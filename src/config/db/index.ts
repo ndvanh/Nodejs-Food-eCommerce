30c00551eaea53c5075c14c008fc89d35f06a551
VietAnh <ndvietanh01122001@gmail.com>
2023-01-01 04:56:08 +0700

    update DB connect

--- src/config/db/index.ts
+++ src/config/db/index.ts
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+require("dotenv").config()
 const connect = async () => {
     try{
-       await mongoose.connect('mongodb://localhost:27017/db_cloudfoodsv')
+       await mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}.mongodb.net/?retryWrites=true&w=majority`,{useNewUrlParser: true, useUnifiedTopology: true })
        console.log('Kết nối tới db thành công')
     }
     catch(err){
