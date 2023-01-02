595a285576184876158728b5d58ba7223e9b88d5
VietAnh <ndvietanh01122001@gmail.com>
2023-01-02 13:26:42 +0700

    update config

--- dist/config/db/index.js
+++ dist/config/db/index.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 const connect = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose.connect('mongodb://localhost:27017/db_cloudfoodsv');
+        yield mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
         console.log('Kết nối tới db thành công');
     }
     catch (err) {
