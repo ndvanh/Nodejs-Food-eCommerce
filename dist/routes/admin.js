9e15d2caef6043ba65e7bbaaa9a86cfc21838109
VietAnh <ndvietanh01122001@gmail.com>
2023-01-02 13:54:21 +0700

    update config

--- dist/routes/admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const router = express.Router();
-const adminController = require('../controllers/AdminController');
-router.post('/login', adminController.loginAdmin);
-module.exports = router;
-//# sourceMappingURL=admin.js.map
